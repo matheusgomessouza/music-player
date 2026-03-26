2fd1c590bd6d8aa4a5583d82e9d3c8e40c9b3234
-m <-m>
2026-03-26 17:17:55 -0300

    feat(app): integrate Vercel Analytics and improve code formatting
    - Added "@vercel/analytics" dependency to package.json for analytics tracking.
    - Updated App.tsx to include the Analytics component for monitoring user interactions.
    - Reformatted code in App.tsx for improved readability and consistency.
    
    These changes enhance the application's tracking capabilities by integrating Vercel Analytics, allowing for better insights into user behavior. Additionally, the code formatting improvements contribute to maintainability and readability, making it easier for future developers to work on the project.

--- projects/frontend/src/App.tsx
+++ projects/frontend/src/App.tsx
@@ -1,18 +1,22 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import UploadMusicPage from './pages/UploadMusicPage';
-import NotFoundPage from './pages/NotFoundPage';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import UploadMusicPage from "./pages/UploadMusicPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/upload" element={<UploadMusicPage />} />
-        <Route path="/404" element={<NotFoundPage />} />
-        <Route path="*" element={<Navigate to="/404" replace />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/upload" element={<UploadMusicPage />} />
+          <Route path="/404" element={<NotFoundPage />} />
+          <Route path="*" element={<Navigate to="/404" replace />} />
+        </Routes>
+      </BrowserRouter>
+      <Analytics />
+    </>
   );
 }
 
